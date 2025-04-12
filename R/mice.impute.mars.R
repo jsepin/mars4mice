d427e4954f7df062b6b991e762e568cfe1d16a81
Sepin Jerome <jerome.sepin@unilu.ch>
2025-04-12 10:07:47 +0200

    add documentation

--- R/mice.impute.mars.R
+++ R/mice.impute.mars.R
@@ -1,24 +1,39 @@
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# MARS imputation using earth package ---- 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#' MARS algorithm to be used within \code{mice}
+#'
+#' Imputes univariate missing data using the MARS algorithm with subsampling.
+#'
+#' @param y Vector to be imputed.
+#' @param ry Logical vector of length \code{length(y)} indicating the the subset \code{y[ry]} of elements in y to which the imputation model is fitted. The \code{ry} generally distinguishes the observed (\code{TRUE}) and missing values (\code{FALSE}) in \code{y}.
+#' @param x Numeric design matrix with \code{length(y)} rows with predictors for \code{y}. Matrix \code{x} may have no missing values.
+#' @param wy Logical vector of length \code{length(y)}. A \code{TRUE} value indicates locations in \code{y} for which imputations are created.
+#' @param degree Maximum degree of interaction. Default is 10.
+#' @param sampling Amount of subsampling that is applied. Default is 0 which corresponds to bootstrap resampling.
+#' @param ... Other named arguments.
+#'
+#' @return Vector with imputed data, same type as \code{y}, and of length \code{sum(wy)}
+#' @export
+#'
+#' @examples
+#' # We normally call mice.impute.mars() from within mice()
+
 
 mice.impute.mars <- function (y, ry, x, wy = NULL, degree = 10, sampling = 0, ...){
   mice:::install.on.demand("earth", ...)
-  if (is.null(wy)) 
+  if (is.null(wy))
     wy <- !ry
   nobs <- sum(ry)
-  
+
   # if sampling==0 then bootstrap otherwise is subsampling
   if(sampling==0){
     s <- sample(1:nobs,size = nobs,replace = TRUE)
   }else{
     s <- sample(1:nobs,size = sampling*nobs,replace = FALSE)
   }
-  
+
   dotxobs <- x[ry, , drop = FALSE][s, , drop = FALSE]
   dotyobs <- y[ry][s]
-  
+
   if(is.factor(dotyobs)){
     suppressWarnings({
     earth.mod <- earth::earth(x = dotxobs, y = dotyobs
@@ -37,7 +52,7 @@ mice.impute.mars <- function (y, ry, x, wy = NULL, degree = 10, sampling = 0, ..
                               , glm=list(family=gaussian)
                               , degree = degree)
     s2hat <- mean((predict(earth.mod, dotxobs) - dotyobs)^2)
-    impute <- as.vector(predict(earth.mod, x[wy,,drop = FALSE])) + 
+    impute <- as.vector(predict(earth.mod, x[wy,,drop = FALSE])) +
       rnorm(sum(wy), 0, sqrt(s2hat))
   }
   return(impute)
